b051f1e34f60583530dac5ae816ded8dd02a87b1
Phyllipe Lima <phyllipe_slf@yahoo.com.br>
2018-04-12 09:20:22 -0300

    Initial commit

---
+++ src/test/java/br/inpe/cap/arthas/test/RepoTest.java
@@ -0,0 +1,14 @@
+package br.inpe.cap.arthas.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RepoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
